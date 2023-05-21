86d87e4a70867b1b7e3a305ef26e47b8ac494b75
tip <tiptop22tip@gmail.com>
2023-05-21 19:07:46 +0530

    added filters and custom actions to product admin

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,29 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models.aggregates import Count
-
+from django.db.models.query import QuerySet
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     autocomplete_fields = ['collection']
+    actions = ['clear_inventory']
+    
     prepopulated_fields = {
         'slug': ['title']
     }
@@ -14,7 +31,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
-    list_filter = ['collection', 'last_update']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
     search_fields = ['title']
@@ -28,6 +45,15 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory < 10:
             return 'Low'
         return 'OK'
+    
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated.',
+            messages.ERROR
+        )
 
 
 @admin.register(models.Collection)
